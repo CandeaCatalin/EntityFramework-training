2279a25e6c5ec913e08a49bae3fba1ce4b3ac5f9
Catalin Candea <catalin.candea@theaccessgroup.com>
2021-10-22 15:43:44 +0300

    Add project files.

---
+++ SamuraiApp.UI/Program.cs
@@ -0,0 +1,44 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+        static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+            //GetSamurais("Before Add:");
+            //AddSamurais("Julie", "Sampson");
+            AddVariousTypes();
+            GetSamurais("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+        private static void AddSamurais(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+            }
+            _context.SaveChanges();
+        }
+        private static void AddVariousTypes()
+        {
+            _context.AddRange(new Samurai { Name = "Marcel" }, new Battle { Name = "Mall Băneasa" });
+            _context.SaveChanges();
+        }
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
